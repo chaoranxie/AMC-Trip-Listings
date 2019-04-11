9146ddf4fd3de7fd905a6e25350f652f29c9fd47
Chao Xie <chaoranxie@gmail.com>
2019-04-11 13:56:29 -0400

    remove set_magic_quotes_runtime because of php7

--- Cache/Lite.php
+++ Cache/Lite.php
@@ -648,7 +648,6 @@ class Cache_Lite
             clearstatcache(); // because the filesize can be cached by PHP itself...
             $length = @filesize($this->_file);
             $mqr = get_magic_quotes_runtime();
-            set_magic_quotes_runtime(0);
             if ($this->_readControl) {
                 $hashControl = @fread($fp, 32);
                 $length = $length - 32;
@@ -658,7 +657,6 @@ class Cache_Lite
             } else {
                 $data = '';
             }
-            set_magic_quotes_runtime($mqr);
             if ($this->_fileLocking) @flock($fp, LOCK_UN);
             @fclose($fp);
             if ($this->_readControl) {
